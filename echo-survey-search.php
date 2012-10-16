645867e72954f9cfb6669cc62f060f03fcd317ae
Guy Radcliffe <gr7432@gmail.com>
2012-10-16 12:39:53 -0400

    added table html

--- echo-survey-search.php
+++ echo-survey-search.php
@@ -45,20 +45,22 @@ $answer = mysql_real_escape_string($_GET['answer']);
 $month = mysql_real_escape_string($_GET['month']);
 $year = mysql_real_escape_string($_GET['year']);
 
-if (!empty($question)&&!empty($answer)&&!empty($month)&&!empty($year)) { // if year & month & question & answer
-		echo "<p><a href='" . $_SERVER['PHP_SELF'] . "'>Start a new search</a></p><p>You searched for <b>" . $answer . "</b>, <b>" . $question . "</b>, <b>" . $year . "</b> and <b>" .  $month . "</b></p>" . PHP_EOL;
+if (empty($question)&&empty($answer)&&empty($month)&&!empty($year)) { // if year & month & question & answer
+		echo "<p><a href='" . $_SERVER['PHP_SELF'] . "'>Start a new search</a></p><br /><p>You searched for <b>" .  $year . "</b>.</p>" . PHP_EOL;
 		$color1 = "#474747"; 
     $color2 = "#2a2a2a"; 
 		$countData = mysql_fetch_assoc(mysql_query("SELECT found_rows() AS total"));
 		$totalRows = $countData['total'];
     //get june's question
     $june_count = 0;
-		$junequestion = mysql_query("SELECT SQL_CALC_FOUND_ROWS * FROM surveyTbl WHERE question='What kind of data on IMGs in U.S. GME would you like to see?' LIMIT 0,1");
+		$junequestion = mysql_query("SELECT SQL_CALC_FOUND_ROWS * FROM surveyTbl WHERE year='2012' AND month='June' AND question='What kind of data on IMGs in U.S. GME would you like to see?' LIMIT 0,1");
+    echo "<table class='survey-table'>" . PHP_EOL;//start table
     while ($row = mysql_fetch_array($junequestion)) {
     	$row_color = ($june_count % 2) ? $color1 : $color2;
     	$june_count++;
     	if ($june_count>0 && $june_count<2) {
-    	  echo "<h5>" . $row[question] . "</h5>" . PHP_EOL;
+    	 echo "<tr><td class='main'><h5><span>Month/Year: </span>" .$row[month] . ", " . $row[year] . "</td></tr>" . PHP_EOL;
+    	 echo "<tr><td class='main'><h5><span>Question: </span>" . $row[question] . "</h5></td></tr>" . PHP_EOL;
     	}
   	}
   	$answerone = mysql_query("SELECT SQL_CALC_FOUND_ROWS * FROM surveyTbl WHERE answer='IMGs in U.S. GME, by country of medical school'") or die ("Could not fetch answer data");
@@ -81,20 +83,21 @@ if (!empty($question)&&!empty($answer)&&!empty($month)&&!empty($year)) { // if y
     $answerfivenum = mysql_num_rows($answerfive);
     $answerfiveprint = mysql_fetch_assoc($answerfive);
     
-    echo "<div style='width:600px; text-align:right;'>" . $answeroneprint['answer'] . ":&nbsp;" . $answeronenum . "</div>". PHP_EOL;
-    echo "<div style='width:600px; text-align:right;'>" . $answertwoprint['answer'] . ":&nbsp;" . $answertwonum . "</div>". PHP_EOL;
-    echo "<div style='width:600px; text-align:right;'>" . $answerthreeprint['answer'] . ":&nbsp;" . $answerthreenum . "</div>". PHP_EOL;
-    echo "<div style='width:600px; text-align:right;'>" . $answerfourprint['answer'] . ":&nbsp;" . $answerfournum . "</div>". PHP_EOL;
-    echo "<div style='width:600px; text-align:right;'>" . $answerfiveprint['answer'] . ":&nbsp;" . $answerfivenum . "</div>". PHP_EOL;
-    echo "<div style='height:25px;'></div>";
+    echo "<tr><td class='main'>" . $answeroneprint['answer'] . ":&nbsp;" . $answeronenum . "</td></tr>" . PHP_EOL;
+    echo "<tr><td class='main'>" . $answertwoprint['answer'] . ":&nbsp;" . $answertwonum . "</td></tr>" . PHP_EOL;
+    echo "<tr><td class='main'>" . $answerthreeprint['answer'] . ":&nbsp;" . $answerthreenum . "</td></tr>" . PHP_EOL;
+    echo "<tr><td class='main'>" . $answerfourprint['answer'] . ":&nbsp;" . $answerfournum . "</td></tr>" . PHP_EOL;
+    echo "<tr><td class='main'>" . $answerfiveprint['answer'] . ":&nbsp;" . $answerfivenum . "</td></tr>" . PHP_EOL;
+    echo "<tr><td class='main'>&nbsp;</td></tr>" . PHP_EOL;
   	//get july's question
   	$july_count = 0;
-  	$julyquestion = mysql_query("SELECT SQL_CALC_FOUND_ROWS * FROM surveyTbl WHERE question='What kind of information about the residency interview process would be of interest to you?' LIMIT 0,1");
+  	$julyquestion = mysql_query("SELECT SQL_CALC_FOUND_ROWS * FROM surveyTbl WHERE year='2012' AND month='July' AND question='What kind of information about the residency interview process would be of interest to you?' LIMIT 0,1");
   	while ($row = mysql_fetch_array($julyquestion)) {
     	$row_color = ($july_count % 2) ? $color1 : $color2;
     	$july_count++;
     	if ($july_count>0 && $july_count<2) {
-    	  echo "<h5>" . $row[question] . "</h5>" . PHP_EOL;
+    	 echo "<tr><td class='main'><h5><span>Month/Year: </span>" .$row[month] . ", " . $row[year] . "</td></tr>" . PHP_EOL;
+    	 echo "<tr><td class='main'><h5><span>Question: </span>" . $row[question] . "</h5></td></tr>" . PHP_EOL;
     	}
   	}
     // Retrieve number of rows of the first answer   
@@ -114,19 +117,20 @@ if (!empty($question)&&!empty($answer)&&!empty($month)&&!empty($year)) { // if y
     $answerfournum = mysql_num_rows($answerfour);
     $answerfourprint = mysql_fetch_assoc($answerfour);
     
-    echo "<div style='width:600px; text-align:right;'>" . $answeroneprint['answer'] . ":&nbsp;" . $answeronenum . "</div>". PHP_EOL;
-    echo "<div style='width:600px; text-align:right;'>" . $answertwoprint['answer'] . ":&nbsp;" . $answertwonum . "</div>". PHP_EOL;
-    echo "<div style='width:600px; text-align:right;'>" . $answerthreeprint['answer'] . ":&nbsp;" . $answerthreenum . "</div>". PHP_EOL;
-    echo "<div style='width:600px; text-align:right;'>" . $answerfourprint['answer'] . ":&nbsp;" . $answerfournum . "</div>". PHP_EOL;
-    echo "<div style='height:25px;'></div>";
+    echo "<tr><td class='main'>" . $answeroneprint['answer'] . ":&nbsp;" . $answeronenum . "</td></tr>". PHP_EOL;
+    echo "<tr><td class='main'>" . $answertwoprint['answer'] . ":&nbsp;" . $answertwonum . "</td></tr>". PHP_EOL;
+    echo "<tr><td class='main'>" . $answerthreeprint['answer'] . ":&nbsp;" . $answerthreenum . "</td></tr>". PHP_EOL;
+    echo "<tr><td class='main'>" . $answerfourprint['answer'] . ":&nbsp;" . $answerfournum . "</td></tr>". PHP_EOL;
+    echo "<tr><td class='main'>&nbsp;</td></tr>" . PHP_EOL;
   	//get august's question
   	$august_count = 0;
-  	$augustquestion = mysql_query("SELECT SQL_CALC_FOUND_ROWS * FROM surveyTbl WHERE question='What questions do you have about U.S. immigration and visa requirements?' LIMIT 0,1");
+  	$augustquestion = mysql_query("SELECT SQL_CALC_FOUND_ROWS * FROM surveyTbl WHERE year='2012' AND month='August' AND question='What questions do you have about U.S. immigration and visa requirements?' LIMIT 0,1");
   	while ($row = mysql_fetch_array($augustquestion)) {
     	$row_color = ($august_count % 2) ? $color1 : $color2;
     	$august_count++;
     	if ($august_count>0 && $august_count<2) {
-    	  echo "<h5>" . $row[question] . "</h5>" . PHP_EOL;
+    	 echo "<tr><td class='main'><h5><span>Month/Year: </span>" .$row[month] . ", " . $row[year] . "</td></tr>" . PHP_EOL;
+    	 echo "<tr><td class='main'><h5><span>Question: </span>" . $row[question] . "</h5></td></tr>" . PHP_EOL;
     	}
   	}
   // Retrieve number of rows of the first answer   
@@ -146,17 +150,17 @@ if (!empty($question)&&!empty($answer)&&!empty($month)&&!empty($year)) { // if y
   $answerfournum = mysql_num_rows($answerfour);
   $answerfourprint = mysql_fetch_assoc($answerfour);
   
-  echo "<div style='width:600px; text-align:right;'>" . $answeroneprint['answer'] . ":&nbsp;" . $answeronenum . "</div>". PHP_EOL;
-  echo "<div style='width:600px; text-align:right;'>" . $answertwoprint['answer'] . ":&nbsp;" . $answertwonum . "</div>". PHP_EOL;
-  echo "<div style='width:600px; text-align:right;'>" . $answerthreeprint['answer'] . ":&nbsp;" . $answerthreenum . "</div>". PHP_EOL;
-  echo "<div style='width:600px; text-align:right;'>" . $answerfourprint['answer'] . ":&nbsp;" . $answerfournum . "</div>". PHP_EOL;
-  echo "<div style='height:25px;'></div>";
+  echo "<tr><td class='main'>" . $answeroneprint['answer'] . ":&nbsp;" . $answeronenum . "</td></tr>". PHP_EOL;
+  echo "<tr><td class='main'>" . $answertwoprint['answer'] . ":&nbsp;" . $answertwonum . "</td></tr>". PHP_EOL;
+  echo "<tr><td class='main'>" . $answerthreeprint['answer'] . ":&nbsp;" . $answerthreenum . "</td></tr>". PHP_EOL;
+  echo "<tr><td class='main'>" . $answerfourprint['answer'] . ":&nbsp;" . $answerfournum . "</td></tr>". PHP_EOL;
+  echo "</table>" . PHP_EOL;
   } else { 
 		echo '<p>Sorry, no matches could be found in the database.</p><p>' . mysql_error() . '</p><br />' . PHP_EOL;
     echo "<p><a href='" . $_SERVER['PHP_SELF'] . "'>Start a new search</a></p><br />" . PHP_EOL; 
 	}
 }
-echo "<p><a href='index.php'>Take a survey</a></p>\n";
+echo "<p><a href='index.php'>Take a survey</a></p><br />" . PHP_EOL;
 echo "</div><!-- end echo-survey -->" . PHP_EOL;
 include ('../common/footer.php');
 ?>
